d15122919a23193538c61284cb8a3468cc166ef3
George Baldini <gebaldini@davidson.edu>
2019-03-21 18:21:18 -0400

    No more error

--- googleCode.py
+++ googleCode.py
@@ -22,7 +22,7 @@ def main():
     for i in range(0, num_students):
         mat.append([])
         for j in range(0, num_classes):
-            mat[i].append(solver.IntVar(0.0, 1.0, str(i)+str(j)))
+            mat[i].append(solver.IntVar(0.0, 1.0, str(i)+" "+str(j)))
 
     #constrain that each student must have between 0 and 4 classes
     constraints = []
@@ -37,43 +37,47 @@ def main():
         course_capacities.append(courses[course])
 
     #constrain that each course must not overflow its capacity
+    shift = len(constraints)
     for col in range(len(mat[0])):
         constraints.append(solver.Constraint(0.0, course_capacities[col]))
         for row in range(len(mat)):
-            constraints[col].SetCoefficient(mat[row][col], 1.0)
+            constraints[shift+col].SetCoefficient(mat[row][col], 1.0)
 
     # Make objective function
     objective = solver.Objective()
     for row in range(len(mat)):
         for col in range(len(mat[0])):
-             #we need to access all of this information
-             #we have the position of the student in the matrix, and the position of the class in the matrix
-             #we need to find the year and the major
-             objective.SetCoefficient(mat[row][col], weight(student_ids[row],  course_crns[col]))
+            #we need to access all of this information
+            #we have the position of the student in the matrix, and the position of the class in the matrix
+            #we need to find the year and the major
+            objective.SetCoefficient(mat[row][col], weight(student_ids[row], course_crns[col]))
     objective.SetMaximization()
 
 
     """Solve the problem and print the solution."""
     result_status = solver.Solve()
     # The problem has an optimal solution.
-    # assert result_status == pywraplp.Solver.OPTIMAL
+    assert result_status == pywraplp.Solver.OPTIMAL
 
     # The solution looks legit (when using solvers other than
     # GLOP_LINEAR_PROGRAMMING, verifying the solution is highly recommended!).
-    # assert solver.VerifySolution(1e-7, True)
+    assert solver.VerifySolution(1e-7, True)
 
-    # print('Number of variables =', solver.NumVariables())
-    # print('Number of constraints =', solver.NumConstraints())
+    print('Number of variables =', solver.NumVariables())
+    print('Number of constraints =', solver.NumConstraints())
 
     # The objective value of the solution.
-    # print('Optimal objective value = %d' % solver.Objective().Value())
-    # print()
+    print('Optimal objective value = %d' % solver.Objective().Value())
+    print()
     # The value of each variable in the solution.
     # variable_list = [x, y]
     #
     # for variable in variable_list:
     #     print('%s = %d' % (variable.name(), variable.solution_value()))
-
+    # for row in range(len(mat)):
+    #     for col in range(len(mat[0])):
+    #         variable = mat[row][col]
+    #         print('%s = %d' % (variable.name(), variable.solution_value()))
 
 # get the weight of how "good" it is to give a student a class, based on its
 # position in the webtree ranking, the class year of the student, and their major
@@ -82,7 +86,7 @@ def weight(student, class_crn):
     if class_crn in ranked[student]:
         score += len(ranked[student]) - ranked[student].index(class_crn)
     else:
-        score -= float("inf")
+        score -= 10000
 
     score *= class_crn
 
